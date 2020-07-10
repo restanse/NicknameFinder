3ae9ae6e63f78090726a530b5af8716405647380
restanse <67417142+restanse@users.noreply.github.com>
2020-07-10 22:19:30 +0300

    Add files via upload

---
+++ NicknameFinder.py
@@ -0,0 +1,92 @@
+import requests
+
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
+
+print(Fore.GREEN + '''
+ /$$   /$$ /$$           /$$                                                   /$$$$$$$$ /$$                 /$$                    
+| $$$ | $$|__/          | $$                                                  | $$_____/|__/                | $$                    
+| $$$$| $$ /$$  /$$$$$$$| $$   /$$ /$$$$$$$   /$$$$$$  /$$$$$$/$$$$   /$$$$$$ | $$       /$$ /$$$$$$$   /$$$$$$$  /$$$$$$   /$$$$$$ 
+| $$ $$ $$| $$ /$$_____/| $$  /$$/| $$__  $$ |____  $$| $$_  $$_  $$ /$$__  $$| $$$$$   | $$| $$__  $$ /$$__  $$ /$$__  $$ /$$__  $$
+| $$  $$$$| $$| $$      | $$$$$$/ | $$  \ $$  /$$$$$$$| $$ \ $$ \ $$| $$$$$$$$| $$__/   | $$| $$  \ $$| $$  | $$| $$$$$$$$| $$  \__/
+| $$\  $$$| $$| $$      | $$_  $$ | $$  | $$ /$$__  $$| $$ | $$ | $$| $$_____/| $$      | $$| $$  | $$| $$  | $$| $$_____/| $$      
+| $$ \  $$| $$|  $$$$$$$| $$ \  $$| $$  | $$|  $$$$$$$| $$ | $$ | $$|  $$$$$$$| $$      | $$| $$  | $$|  $$$$$$$|  $$$$$$$| $$      
+|__/  \__/|__/ \_______/|__/  \__/|__/  |__/ \_______/|__/ |__/ |__/ \_______/|__/      |__/|__/  |__/ \_______/ \_______/|__/      
+
+''')
+print('''..........................................................................................coded by https://github.com/restanse
+
+''')
+                                                                             
+nick = input("enter nickname: ")
+prot = "https://"
+
+
+
+snm = {"Instagram": "instagram.com/", "VK": "vk.com/", "Twitter": "twitter.com/",
+"OK": "ok.ru/", "Facebook": "www.facebook.com/", "Telegram": "t.me/", 
+"TikTok": "www.tiktok.com/@", "Mamba": "www.mamba.ru/", "Badoo": "badoo.com/profile/", 
+"Patreon": "patreon.com/", "TamTamChat": "tt.me/", "Teletype": "teletype.in/@"}
+
+
+
+vh = {"YouTube": "youtube.com/user/", 
+"Twitch": "twitch.tv/", "Pornhub": "rt.pornhub.com/users/", "Red Tube": "ru.redtube.com/users/", "Xvideos": "www.xvideos.com/profiles/"}
+
+
+
+games = {"Steam": "steamcommunity.com/id/", "Ubisoft": "club.ubisoft.com/profile/", 
+"Chess": "www.chess.com/ru/member/", "Xbox": "account.xbox.com/profile?gamertag=", "Warface": "wfts.su/profile/"}
+
+
+money = {"DonationAlerts": "www.donationalerts.com/r", "Qiwi": "qiwi.me/", "PayPal": "paypal.me/"}
+
+forums = {"Anime planet": "anime-planet.com/users/", "Linux": "www.linux.org/members/", 
+"Bitcoinforum": "bitcoinforum.com/profile/", "Scala lang": "users.scala-lang.org/u/",
+ "Pikabu": "pikabu.ru/@", "Signalusers": "community.signalusers.org/u/"
+
+}
+
+
+
+other = {"Github": "github.com/", "Ebay": "www.ebay.com/usr/", "Gitlab": "gitlab.com/", 
+"AskFM": "ask.fm/", 
+"PyPi": "pypi.org/user/", "Wikipedia": "wikipedia.org/wiki/user:", "Pastebin": "pastebin.com/u/", 
+"Metacritic": "metacritic.com/user/", "OpenStreetMap": "www.openstreetmap.org/user/", 
+"Pinboard": "pinboard.in/u:", "Pinkbike": "pinkbike.com/u/",
+"Seoclerks": "www.seoclerks.com/user/"}
+
+
+
+def osint(dict):
+  for key, site in dict.items():
+	  url = prot + site + nick
+	  try:
+	   r = requests.get(url)
+	
+	   if r.status_code == 200:
+	       print(Fore.GREEN + "found " + key + ": " + url)
+	   else:
+		   print(Fore.RED + key + " not found")
+	  except:
+	   print(Fore.YELLOW + "request error for " + key)
+
+print(Fore.WHITE + "social networks and messangers:")
+osint(snm)
+
+print(Fore.WHITE + "videohostings:")
+osint(vh)
+
+print(Fore.WHITE + "games:")
+osint(games)
+
+
+print(Fore.WHITE + "forums: ")
+osint(forums)
+
+print(Fore.WHITE + "money: ")
+osint(money)
+
+print(Fore.WHITE + "other sites: ")
+osint(other)
